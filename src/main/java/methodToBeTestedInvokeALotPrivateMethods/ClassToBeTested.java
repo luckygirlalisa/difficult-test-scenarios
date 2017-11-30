ee12634680e81bb29d370a3a0dc9504c68835637
zfxiao <luckygirlalisa@gmail.com>
2017-11-30 13:12:55 +0800

    Powermock: spy class to be tested and make private method do nothing.

--- src/main/java/methodToBeTestedInvokeALotPrivateMethods/ClassToBeTested.java
+++ src/main/java/methodToBeTestedInvokeALotPrivateMethods/ClassToBeTested.java
@@ -1,6 +1,7 @@
 package methodToBeTestedInvokeALotPrivateMethods;
 
 public class ClassToBeTested {
+    private String flag = "initial";
 
     public String toBeTested() {
         boolean interAction = someActionInThisClass();
@@ -10,6 +11,17 @@ public class ClassToBeTested {
 
     private boolean someActionInThisClass() {
 
+        throw new UnsupportedOperationException();
+    }
+
+    public String updateFlag(String updatedFlag) {
+        System.out.println("update flag is called");
+        someAction(updatedFlag);
+        return flag;
+    }
+
+    private void someAction(String updatedFlag) {
+        flag = updatedFlag;
         throw new UnsupportedOperationException();
     }
 }
--- src/test/java/methodToBeTestedInvokeALotPrivateMethods/ClassToBeTestedTest.java
+++ src/test/java/methodToBeTestedInvokeALotPrivateMethods/ClassToBeTestedTest.java
@@ -6,7 +6,10 @@ import org.powermock.api.mockito.PowerMockito;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
 
-import static org.junit.Assert.*;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
+import static org.mockito.Matchers.anyString;
 
 @RunWith(PowerMockRunner.class)
 @PrepareForTest(ClassToBeTested.class)
@@ -19,5 +22,15 @@ public class ClassToBeTestedTest {
         PowerMockito.doReturn(true).when(spyClassToBeTested, "someActionInThisClass");
 
         assertEquals("Inter action excuted: true", spyClassToBeTested.toBeTested());
+    }
+
+    @Test
+    public void shouldStubPrivateMethodWithOutReturn() throws Exception {
+        ClassToBeTested classToBeTested = PowerMockito.spy(new ClassToBeTested());
+
+        PowerMockito.doNothing().when(classToBeTested, "someAction", anyString());
+        String updated = classToBeTested.updateFlag("updated");
+
+        assertThat("flag is not updated since method inside update flag is not called", updated, is("initial"));
     }
 }
